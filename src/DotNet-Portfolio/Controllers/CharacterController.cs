589bc5e06e9160e76f8f60972753f7e852ab07ba
David Wilson <davidtheadmiral@gmail.com>
2017-08-25 14:53:13 -0700

    filler content, add mui, ensure layout is appropriate, styling

--- src/DotNet-Portfolio/Controllers/CharacterController.cs
+++ src/DotNet-Portfolio/Controllers/CharacterController.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using DotNet_Portfolio.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DotNet_Portfolio.Controllers
 {
-    public class HomeController : Controller
+    public class CharacterController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
-        {
-            
-            return View();
-        }
-        public IActionResult About()
         {
             return View();
         }
--- src/DotNet-Portfolio/Controllers/ProjectsController.cs
+++ src/DotNet-Portfolio/Controllers/ProjectsController.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DotNet_Portfolio.Models;
+using Newtonsoft.Json.Linq;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DotNet_Portfolio.Controllers
 {
-    public class HomeController : Controller
+    public class ProjectsController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult GitStars()
         {
-            
-            return View();
+            //Github.TestGitAPI();//works
+            //Github.GetGitRepos();//works, returns a JArray of 30 repos
+            JObject githubRepos = Github.GetStarredRepos();
+            return View(githubRepos);
         }
-        public IActionResult About()
+        public IActionResult DigitalEase()
         {
             return View();
         }
